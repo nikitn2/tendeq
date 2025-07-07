84f6ca1c62907d40d9f14b5183b1d874f465789f
Nik Gourianov <nikgourianov@icloud.com>
2025-07-07 15:42:54 +0200

    slight adjustment to print statement

--- TnMachinery/MPSfunctions.py
+++ TnMachinery/MPSfunctions.py
@@ -950,7 +950,7 @@ def mpsConcatenate(mpses_in, max_bond=None, cutoff=1e-14):
         if mpses_out == None: mpses_out = mps_out
         else:                 mpses_out.add_MPS(mps_out, inplace=True, compress=False)
         if i % 20 == 0:
-            print("MPS-concat {:.1f}% complete.".format(i / len(mpses_in) * 100.0))
+            print("MPS-concat {:.1f}% complete.".format(i / len(mpses_in) * 100.0),flush=True)
             mpses_out.compress(max_bond=max_bond, cutoff=cutoff)
             mpses_out.show()
 
